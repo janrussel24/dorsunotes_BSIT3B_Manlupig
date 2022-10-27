672b148c8550445c08550ddc5bea38b3e8960c88
Jan Russel Sabillo Manlupig <112072074+janrussel24@users.noreply.github.com>
2022-10-27 08:48:52 +0800

    Update Developers.tsx

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -4,17 +4,12 @@ const Developers = () => {
   return (
     <div className='container mx-auto p-5'>
         <h1>Developers</h1>
-        <div className='p-5 m-5 grid grid-cols-6'>  
-            <div className="flex flex-wrap justify-center">
-             
-              <div className="w-64 sm:w-64 px-4">
-                        <img src="MANLUPIG.png" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
-                    </div>
-                    <h3 className='font-bold mr-1'>Jan Russel S. Manlupig, </h3>
-                    <p className='font-thin italic'>Student Developer</p>
-                </div>
-               
+        <div className='p-5 m-5 grid grid-cols-6'> 
+             {/* Add your profile here. You can upload your image to the repo under "images"  directory
+                    and link it here.                
+                */} 
 
+                
                 {/* grid item */}
                 <div className="flex flex-wrap justify-center">
                     <div className="w-64 sm:w-64 px-4">
@@ -22,9 +17,18 @@ const Developers = () => {
                     </div>
                     <h3 className='font-bold mr-1'>John Paulin, </h3>
                     <p className='font-thin italic'>Lead Developer</p>
-                </div>           
-                {/* grid item */}               
-               
+                </div>    
+                     
+                <div className="flex flex-wrap justify-center">
+                    <div className="w-64 sm:w-64 px-4">
+                        <img src="https://avatars.githubusercontent.com/u/74034797?s=400&u=8d250db9bceb4f7415526ea5b276c203f10e3fa9&v=4" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                    </div>
+                    <h3 className='font-bold mr-1'>Jan Russel S. Manlupig, </h3>
+                    <p className='font-thin italic'>Student Developer</p>
+                </div>
+                 
+                 
+                 {/* grid item */}
 
         </div>
     </div>
